fd84fc6432f0fbcebdc94a2951f248ca40b92a7b
Tek9x <none@none.com>
2015-12-27 01:11:41 -0500

    adding functionality22

--- DataBuilder.py
+++ DataBuilder.py
@@ -1,11 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('http://www.wtso.cc/video/vic/eng/season_2?lang=en')
+r = requests.get('http://www.wtso.cc/video/vic/eng/season_3?lang=en')
 data = r.text
 soup = BeautifulSoup(data, 'lxml')
 
 episodeurls = soup.select("div.entryBlock [href]")
 
+
 if episodeurls:
-    print [link['href'] for link in episodeurls]
+    seasons = [link['href'] for link in episodeurls]
+    print seasons
+
+for i in seasons:
+    g = requests.get(i)
+    test = g.text
+    so = BeautifulSoup(test, 'lxml')
+    dlinks = so.select("#content_full > div > h1")
+    store = dlinks[0].get_text().strip()
+    stop = str(store).lstrip('Season 1234567890 episode ')
+    print str(stop).lstrip('-')
