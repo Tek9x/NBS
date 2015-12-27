9bcb0b3a6191163291c389578903de03272f2340
Tek9x <none@none.com>
2015-12-27 01:19:59 -0500

    adding functionality22

--- DataBuilder.py
+++ DataBuilder.py
@@ -11,12 +11,16 @@ episodeurls = soup.select("div.entryBlock [href]")
 if episodeurls:
     seasons = [link['href'] for link in episodeurls]
     print seasons
-
+title = []
 for i in seasons:
+
     g = requests.get(i)
     test = g.text
     so = BeautifulSoup(test, 'lxml')
     dlinks = so.select("#content_full > div > h1")
     store = dlinks[0].get_text().strip()
     stop = str(store).lstrip('Season 1234567890 episode ')
-    print str(stop).lstrip('-')
+    doh = str(stop).lstrip('-')
+    title.append(doh)
+
+print tedst
